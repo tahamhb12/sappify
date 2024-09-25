a6b915f57f18a21a67d7be6cf41e388b0129e260
Taha Mhb <tahamhb12@gmail.com>
2024-09-25 15:51:22 +0100

    changed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\PartnerController;
+use App\Http\Controllers\ShopifyAppController;
+use App\Http\Controllers\ShopifyAppEventController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/shopify/app', [ShopifyAppController::class, 'App']);
+Route::get('/shopify/store', [ShopifyAppController::class, 'store']);
+
+Route::get('/shopify/event', [ShopifyAppEventController::class, 'Events']);
+Route::get('/shopify/event/store', [ShopifyAppEventController::class, 'store']);
